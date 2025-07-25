82db7cc3381625d321f61421dad7f3d013ee4938
Jinyzzz <go56lay@mytum.de>
2025-07-25 15:27:25 +0200

    EORI

--- src/create_xml.py
+++ src/create_xml.py
@@ -547,11 +547,11 @@ def report_intro(root, prepared_data, test_report):
         elif general_info["importer_country"] == "DE":
             root["ReferenceNumber"] = "DE000004"
         elif general_info["importer_country"] == "AT":
-            root["ReferenceNumber"] = "DE000004"
+            root["ReferenceNumber"] = "AT000038"
         elif general_info["importer_country"] == "FR":
             root["ReferenceNumber"] = "FR000001"
         elif general_info["importer_country"] == "IT":
-            root["ReferenceNumber"] = "IT000001"
+            root["ReferenceNumber"] = "IT000005"
         else:
             Log.error(f"[national_competent_auth] NathCompAuths other then FR, DE and AT not supporter yet. Customer country: '{general_info['importer_country']}")
         return root
